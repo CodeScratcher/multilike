e97b84a5a0206f05c389ced5e74b3b2f146d7917
Arielle Fisher <bb8andr2d2forlife@gmail.com>
2023-06-01 06:26:56 -0400

    Launched the server using Vert.X

--- server/src/com/inot/multilike/ServerLauncher.java
+++ server/src/com/inot/multilike/ServerLauncher.java
@@ -1,7 +1,28 @@
 package com.inot.multilike;
 
+import io.vertx.core.net.NetServer;
+import io.vertx.core.net.NetServerOptions;
+import io.vertx.core.Vertx;
+
 public class ServerLauncher {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vertx vertx = Vertx.vertx();
+
+        NetServerOptions options = new NetServerOptions().setPort(4321);
+        NetServer server = vertx.createNetServer(options);
+
+        server
+            .listen()
+            .onComplete(res -> {
+                if (res.succeeded()) {
+                    System.out.println("Server is now listening!");
+                }
+                else {
+                    System.out.println("Failed to bind!");
+                }
+                }
+            );
+
+
     }
 }
