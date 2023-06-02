8ee18e90049cc11d7da1a8870f68480f77e9f82c
Arielle <afisher2026@jpkeefehs.org>
2023-06-02 10:32:55 -0400

    Added entity system

---
+++ core/src/com/inot/multilike/entity/Entity.java
@@ -0,0 +1,7 @@
+package com.inot.multilike.entity;
+
+import com.inot.multilike.model.GameState;
+
+public interface Entity {
+    void update(GameState state);
+}
---
+++ core/src/com/inot/multilike/model/GameState.java
@@ -0,0 +1,9 @@
+package com.inot.multilike.model;
+
+import com.inot.multilike.entity.Entity;
+
+import java.util.List;
+
+public class GameState {
+    List<Entity> entityList;
+}
