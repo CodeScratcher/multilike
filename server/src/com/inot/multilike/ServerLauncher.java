04934c4722b5d1f58ac540d759371210da1506ca
Arielle Fisher <bb8andr2d2forlife@gmail.com>
2023-07-16 19:03:57 -0400

    Multiplayer framework setup - server now goes through LibGDX to manage frame rate / consistent update speed

--- server/src/com/inot/multilike/ServerLauncher.java
+++ server/src/com/inot/multilike/ServerLauncher.java
@@ -1,5 +1,7 @@
 package com.inot.multilike;
 
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import io.vertx.core.net.NetServer;
 import io.vertx.core.net.NetServerOptions;
 import io.vertx.core.Vertx;
@@ -10,31 +12,7 @@ import java.util.List;
 
 public class ServerLauncher {
     public static void main(String[] args) {
-        Vertx vertx = Vertx.vertx();
-
-        List<NetSocket> sockets = new ArrayList<>();
-        NetServerOptions options = new NetServerOptions().setPort(4321);
-        NetServer server = vertx.createNetServer(options);
-
-        server.connectHandler(socket -> {
-            sockets.add(socket);
-            socket.handler(buffer -> {
-
-                for (NetSocket sock : sockets) {
-                    System.out.println(buffer);
-                    sock.write(buffer);
-                }
-            });
-        });
-
-        server
-            .listen()
-            .onComplete(res -> {
-                if (res.succeeded()) {
-                    System.out.println("Server is now listening!");
-                } else {
-                    System.out.println("Failed to bind!");
-                }
-            });
+        HeadlessApplicationConfiguration headlessApplicationConfiguration = new HeadlessApplicationConfiguration();
+        new HeadlessApplication(new MultiLikeServer(), headlessApplicationConfiguration);
     }
 }
